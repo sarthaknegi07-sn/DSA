b85762bc8d9342b6ea610f382d131a309bf11e78
Sarthak Negi <sarthakn1122@gmail.com>
2026-07-29 00:56:16 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (99.77%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -11,36 +11,21 @@
  */
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) {
+    vector<int>ds;      // STORES THE RIGHT VIEW    
+    void revPreorder(TreeNode * root,int level){
         if(root==NULL)
-            return {};
+            return;
+
+        if(level==ds.size())
+            ds.push_back(root->val);
         
-        // PERFORM LEVEL ORDER TRAVERSAL
-        queue<TreeNode *>q;
-        vector<vector<int>>res;
+        revPreorder(root->right,level+1);
+        revPreorder(root->left,level+1);
+    }
 
-        q.push(root);
-        while(!q.empty()){
-            int size=q.size();
-            vector<int>level;
-            for(int i=0;i<size;i++){
-                TreeNode * x=q.front();
-                q.pop();
-                level.push_back(x->val);
-                if(x->left)
-                    q.push(x->left);
-                if(x->right)
-                    q.push(x->right);
-            }
-            res.push_back(level);
-        }
-
-        // NOW ONLY TAKE THE LAST NODE OF EVERY LEVEL
-        vector<int>ans;
-        for(auto it:res){
-            ans.push_back(it[it.size()-1]);
-        }
-
-        return ans;
+    vector<int> rightSideView(TreeNode* root) {
+        // RECURSIVE APPROACH 
+        revPreorder(root,0);
+        return ds;
     }
 };
