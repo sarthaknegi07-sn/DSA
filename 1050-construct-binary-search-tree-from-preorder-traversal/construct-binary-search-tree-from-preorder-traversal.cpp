383b6c900e3f02f3778649d472366efa048acb07
Sarthak Negi <sarthakn1122@gmail.com>
2026-08-04 20:58:57 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (21.51%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -6,48 +6,23 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right)
- *         : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
-    TreeNode* insert(TreeNode* root, int val) {
-        if (root == NULL)
-            return new TreeNode(val);
 
-        TreeNode* curr = root;
-
-        while (true) {
-            if (val < curr->val) {
-                if (curr->left == NULL) {
-                    curr->left = new TreeNode(val);
-                    break;
-                }
-                curr = curr->left;
-            } else {
-                if (curr->right == NULL) {
-                    curr->right = new TreeNode(val);
-                    break;
-                }
-                curr = curr->right;
-            }
-        }
-
-        return root;
+    TreeNode* build(vector<int>& preorder,int & i,int bound){
+        if(i==preorder.size() || preorder[i]>bound)
+            return NULL;
+        TreeNode* root=new TreeNode(preorder[i++]);
+        root->left=build(preorder,i,root->val);
+        root->right=build(preorder,i,bound);
+        return root; 
     }
 
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        if (preorder.empty())
-            return NULL;
-
-        TreeNode* root = new TreeNode(preorder[0]);
-
-        for (int i = 1; i < preorder.size(); i++) {
-            insert(root, preorder[i]);
-        }
-
-        return root;
+        int i=0;
+        return build(preorder,i,INT_MAX);
     }
 };
