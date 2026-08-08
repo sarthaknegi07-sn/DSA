fe72d2c300b0f393217e53ad9065d6f5161ed16d
Sarthak Negi <sarthakn1122@gmail.com>
2026-08-08 13:38:25 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (51.60%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -8,6 +8,7 @@ public:
         // {{r,c},t}
         queue<pair< pair<int,int>, int>>q;
         
+        int cntfresh=0;
         // first make visited array
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
@@ -17,12 +18,16 @@ public:
                 }
                 else 
                     vis[i][j]=0;
+                if(grid[i][j]==1)   // this means the apple is fresh
+                    cntfresh++;
             }
         }
-        // now the first rotten is pushed in queue
+        // now as the first rotten is pushed in queue
         int tm=0; 
+        // for checking the adjacency apples
         int drow[]={-1,0,1,0};
         int dcol[]={0,1,0,-1};
+        int cnt=0;
         while(!q.empty()){
             int r=q.front().first.first;
             int c=q.front().first.second;
@@ -33,21 +38,18 @@ public:
             for(int i=0;i<4;i++){   // going to all 4 directions
                 int nrow=r+drow[i];
                 int ncol=c+dcol[i];
-                // now check if it is not out of bound, not visited and fresh
+                // now check if the apple is not out of bound, not visited and a fresh apple
                 if(nrow>=0 && nrow<m && ncol>=0 && ncol<n 
                 && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
                     q.push({{nrow,ncol},t+1});
                     vis[nrow][ncol]=2;
+                    cnt++;
                 }
             }
         }
-        // check whether some apples aren't rotten
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(vis[i][j]!=2 && grid[i][j]==1)
-                    return -1;
-            }
-        }
+        // check whether all fresh apples are rotten
+        if(cntfresh!=cnt)
+            return -1;
         return tm;
     }
 };
