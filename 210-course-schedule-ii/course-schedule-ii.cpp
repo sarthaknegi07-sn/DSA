19c3e0bb7249644fc886f2b1ecd2dff035bff4e6
Sarthak Negi <sarthakn1122@gmail.com>
2026-08-14 19:00:58 +0530

    Time: 3 ms (70.79%) | Memory: 18.4 MB (56.67%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,46 +1,46 @@
 class Solution {
 public:
-
-    bool dfs(int node,vector<int>& vis,vector<int>& pathvis,vector<vector<int>>& adj,vector<int>& ans){
-        vis[node]=1;
-        pathvis[node]=1;
-
-        for(auto it:adj[node]){
-            if(!vis[it]){
-                if(dfs(it,vis,pathvis,adj,ans)==true)   // cycle exists
-                    return true;
-            }
-            // visited 
-            else if(pathvis[it])    //cycle exists
-                return true;
-        }
-
-        pathvis[node]=0;
-        ans.push_back(node);
-        return false;   // no cycle
-    }
-
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        // check if a cycle exists in a directed graph
-        vector<int>vis(numCourses,0);
-        vector<int>pathvis(numCourses,0);
-        vector<int>ans;
+        // PERFORMING IT USING TOPO SORT
 
-        // now make adjacency list
-        vector<vector<int>> adj(numCourses);
+        vector<int>adj[numCourses];
         for(auto it:prerequisites){
             adj[it[1]].push_back(it[0]);
         }
 
-        // now perform dfs
+        // Now perform topo sort and if topo.size() == N -> cycle doesn't exists
+
+        queue<int>q;
+
+        // first find indegree
+        vector<int>indegree(numCourses);
         for(int i=0;i<numCourses;i++){
-            if(!vis[i]){
-                if(dfs(i,vis,pathvis,adj,ans)==true)    // cycle exists
-                    return {};
+            for(auto it:adj[i]){
+                indegree[it]++;
             }
         }
-        // if cycle not exists then true!
-        reverse(ans.begin(),ans.end());
-        return ans;
+
+        // insert into q with indegree=0
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0)
+                q.push(i);
+        }
+
+        vector<int>topo;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+            // now node is in your topo sort so remove it from the indegree
+            for(auto it :adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0)
+                    q.push(it);
+            }
+        }
+
+        if(topo.size() == numCourses)
+            return topo;
+        return {};
     }
 };
