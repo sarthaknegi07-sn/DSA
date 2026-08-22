2392030850068f28f8f226f9a02367b7960a02da
Sarthak Negi <sarthakn1122@gmail.com>
2026-08-23 00:04:38 +0530

    Time: 36 ms (29.35%) | Memory: 25.4 MB (24.88%) - LeetSync

---
+++ 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxDistinct(string s) {
+        unordered_set<int>st;
+        for(auto it:s){
+            st.insert(it);
+        }
+        return st.size();
+    }
+};
