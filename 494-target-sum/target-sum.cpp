e63380cecfea572971900f60cf6e5bf30a51bbcb
Sarthak Negi <sarthakn1122@gmail.com>
2026-09-08 14:58:16 +0530

    Time: 15 ms (50.73%) | Memory: 25.8 MB (25.38%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    int fn(int ind,int sum,int target,vector<int>&nums){
+    int fn(int ind,int sum,int target,vector<int>&nums,vector<vector<int>>& dp,int total){
         if(ind==nums.size()){
             if(sum==target)
                 return 1;
             return 0;
         }
+        if(dp[ind][sum+total]!=-1)    return dp[ind][sum+total];
 
-        int plus=fn(ind+1,sum+nums[ind],target,nums);
-        int minus=fn(ind+1,sum-nums[ind],target,nums);
+        int plus=fn(ind+1,sum+nums[ind],target,nums,dp,total);
+        int minus=fn(ind+1,sum-nums[ind],target,nums,dp,total);
 
-        return plus+minus;
+        return dp[ind][sum+total]=plus+minus;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return fn(0,0,target,nums);
+        // find total sum
+        int total=0;
+        for(int it:nums)
+            total+=it;
+        vector<vector<int>>dp(nums.size(),vector<int>(2*total+1,-1));
+        return fn(0,0,target,nums,dp,total);
     }
 };
